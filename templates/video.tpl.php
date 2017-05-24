fe8f40a2ce83656e5017287fb46d27ef62434324
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-05-24 13:19:09 -0400

    remove __FILE__

--- templates/video.tpl.php
+++ templates/video.tpl.php
@@ -1,4 +1,3 @@
-<h1><?php print __FILE__ ?></h1>
 <div id="<?php print $identifier ?>" data-fullscreen="true" class="flowplayer">
   <video>
     <source type="application/x-mpegurl" src="<?php print $m3u ?>">
