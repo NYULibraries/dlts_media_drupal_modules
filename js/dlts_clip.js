7213f5d682d76f838ed3e7fdcf7d0a83b64f81d7
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-05-25 16:39:12 -0400

    Updates

--- js/dlts_clip.js
+++ js/dlts_clip.js
@@ -1,19 +1,22 @@
 ;(function ($) {
   function attach (context, settings) {
-    flowplayer(function (api, root) {
-      var parent = $(root);
-      $('.fp-playlist a').click(function () {
-        var start = $(this).attr('data-start');
-        var duration = $(this).attr('data-duration');
-        parent.attr({ 'data-start': start, 'data-duration': duration });
-      });
-      api.bind('ready', function (elem) {
-        var start = $(elem.currentTarget).attr('data-start');
-        if (start) {
-          api.seek(start);
-        }
-      });
-    });
+    if (window.flowplayer) {
+      flowplayer(function (api, root) {
+        var parent = $(root);
+        $('.fp-playlist a').click(function () {
+          parent.attr({ 
+            'data-start': $(this).attr('data-start'),
+            'data-duration': $(this).attr('data-duration')
+          })
+        })
+        api.bind('ready', function (elem) {
+          var start = $(elem.currentTarget).attr('data-start')
+          if (start) {
+            api.seek(start)
+          }
+        })
+      })
+    }
   }
   Drupal.behaviors.dlts_clip = { attach: attach };
 })(jQuery);
