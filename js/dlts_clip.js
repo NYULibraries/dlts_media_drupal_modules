6efc89beaa99f461cab5643bc859573036900b16
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-07-26 16:11:06 -0400

    test js api

--- js/dlts_clip.js
+++ js/dlts_clip.js
@@ -8,24 +8,39 @@
         share: false
       }
       flowplayer(function (api, root) {
+        
         var parent = $(root);
+        
+        var fsnext = root.querySelector(".fp-next");
+        
+        var fsprev = root.querySelector(".fp-prev");        
+        
         $('.fp-playlist a').click(function () {
           parent.attr({ 
             'data-start': $(this).attr('data-start'),
             'data-duration': $(this).attr('data-duration')
           })
         })
+        
         api.bind('error', function (event, api, error) {
           console.log(error.message + ': ' + error.video.url)
           console.log(error)
         })
+        
         api.bind('ready', function (elem, api, media) {
           var start = $(elem.currentTarget).attr('data-start')
           // https://github.com/video-dev/hls.js/blob/v0.7.8/doc/API.md
+          
+          root.querySelector(".fp-controls").appendChild(fsnext);
+
+          root.querySelector(".fp-controls").appendChild(fsprev);          
+          
           if (start) {
             api.seek(start)
           }
         })
+
+        
       })
     }
   }
