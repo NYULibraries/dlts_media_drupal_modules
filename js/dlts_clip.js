9b7757ec269b2564b87a69a027ab2a3250c4fd10
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-07-28 15:28:43 -0400

    key for nyu.edu

--- js/dlts_clip.js
+++ js/dlts_clip.js
@@ -2,46 +2,32 @@
   function attach (context, settings) {
     if (window.flowplayer) {
       flowplayer.conf = {
-        //key: '#$623666ca2f667514416',
         //logo: "https://flowplayer.com/media/img/mylogo.png",
         share: false
       }
       flowplayer(function (api, root) {
-        
         var parent = $(root)
-
         var controls = parent.find('.fp-controls')
-          
         parent.find('.fp-next').clone().appendTo(controls)
-          
         parent.find('.fp-prev').clone().appendTo(controls)
-        
         $('.fp-playlist a').click(function () {
           parent.attr({ 
             'data-start': $(this).attr('data-start'),
             'data-duration': $(this).attr('data-duration')
           })
         })
-        
         api.bind('error', function (event, api, error) {
           console.log(error.message + ': ' + error.video.url)
           console.log(error)
         })
-        
         api.bind('ready', function (elem, api, media) {
-
           var start = $(elem.currentTarget).attr('data-start')
-              
           if (start) {
             api.seek(start)
           }
-
         })
-        
       })
     }
   }
-  
   Drupal.behaviors.dlts_clip = { attach: attach }
-
-})(jQuery);
+})(jQuery)
